2ada2b77c1d1aba26db945f95d92b18af82b65f9
unknown <caionsantos2000@gmail.com>
2019-12-31 09:58:55 -0300

    adde sidebar links

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(session({
 }))
 app.use(flash())
 
-//midlewares
+//midllewares
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg')
     res.locals.error_msg = req.flash('error_msg')
