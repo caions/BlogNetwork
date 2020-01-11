842e39bb7d7039b4b8e23baddd13060a38b52623
unknown <caionsantos2000@gmail.com>
2020-01-11 08:52:22 -0300

    fix db

--- db/config.js
+++ db/config.js
@@ -6,8 +6,8 @@ const sequelize = new Sequelize('blogNetwork', 'root', '123456', {
     dialect: 'mysql'
 });*/
 
-const sequelize = new Sequelize(process.env.HEROKU_DATABASE_NAME, process.env.HEROKU_USERNAME, process.env.HEROKU_PASS, {
-  host: process.env.HEROKU_HOST,
+const sequelize = new Sequelize('heroku_9b1c68a01410f74', 'bdd2280da2e082', '9623cd32', {
+  host: 'us-cdbr-iron-east-05.cleardb.net',
   dialect: 'mysql'
 });
 
